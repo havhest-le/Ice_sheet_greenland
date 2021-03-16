1272280ae7ae69c2b43e38e24e2f5d4a18c14a49
havhest-le <79570711+havhest-le@users.noreply.github.com>
2021-03-16 22:08:20 +0100

    Update Ice_sheet_greenland.R

--- Ice_sheet_greenland.R
+++ Ice_sheet_greenland.R
@@ -33,12 +33,10 @@ plot(greenland_shape)
 files_runoff <- list.files(path = "C:/Users/Vivi/Documents/Uni/5. Semester/Wasser/Daten/Runoff", pattern = "*.tif", all.files = T, full.names = T)
 files_snowfall <- list.files(path = "C:/Users/Vivi/Documents/Uni/5. Semester/Wasser/Daten/Snowfall", pattern = "*.tif", all.files = T, full.names = T)
 
-
 run <- do.call("rbind", lapply(1:length(files_runoff), function(x){
   tep <- raster(files_runoff[x])
-  ext <- crop(tep, greenland_shape)
-  stat <- quantile(ext, probs = c(0.275, 0.5, 0.975), na.rm = T)
-  data.frame(stat = stat, month = as.numeric(substring(names(tep), 7,8)),
+  med <- cellStats(crop(tep, greenland_shape), stat = 'mean', na.rm = TRUE)
+  data.frame(med = med, month = as.numeric(substring(names(tep), 7,8)),
              year = as.numeric(substring(names(tep), 2,5)))}))
 head(run)
 date = as.POSIXct(glue("{run$year}-01-01"), tz = "GMT") + run$day*24*60*60
@@ -46,24 +44,26 @@ date = as.POSIXct(glue("{run$year}-01-01"), tz = "GMT") + run$day*24*60*60
 
 snow <- do.call("rbind", lapply(1:length(files_snowfall), function(z){
   tep <- raster(files_snowfall[z])
-  ext <- crop(tep, greenland_shape)
-  stat <- quantile(ext, probs = c(0.275, 0.5, 0.975), na.rm = T)
-  data.frame(stat = stat, month = as.numeric(substring(names(tep), 10,12)),
+  med <- cellStats(crop(tep, greenland_shape), stat = 'mean', na.rm = TRUE)
+  data.frame(med = med, month = as.numeric(substring(names(tep), 10,12)),
              year = as.numeric(substring(names(tep),4,7)))}))
 head(snow)
 date = as.POSIXct(glue("{snow$year}-01-01"), tz = "GMT") + snow$day*24*60*60
 
 
-for(y in 2000:2005){
+data <- full_join(snow, run, by = c("month", "year"))
+data <- data[, c(2,3,1,4)]
+data <- rename(data, med_snow = med.x, med_run = med.y)
+data$diff <- ((data$med_snow-data$med_run))
+
+plot_dif <- 
+  ggplot(data = data, mapping = aes(x = year, y = diff))+
+  geom_smooth(stat = "smooth", se = T, size = 1) +
+  theme_minimal()
   
-cat(glue("Wir befinden uns im Jahre {y} nach Christus. Ganz Gallien ist nicht mehr von den Römern besetzt."))
   
-subTab <- subset(run, as.numeric(format(date, "%Y")) %in% y &
-                     as.numeric(format(date, "%m")) %in% c(1:12)) # Monat 6 bis 8 bei mehreren Jahren und Monaten
+  
 
 
 
-  
-    
-}  
 
